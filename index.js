031cb27d2c8308b8bdad333b3154bac3b739d06e
Andrew Visokih <andrew.visokih@gmail.com>
2018-05-16 13:30:46 +0300

    add edit function products

--- index.js
+++ index.js
@@ -70,6 +70,9 @@ $('body').on('change', '.inputEditNew', function(e){
             }
         })
     }
+
+
+
 //console.log(newValueInput);
 })
 //function editing categories
@@ -80,12 +83,34 @@ $('body').on('click', '.editTrCat', function(e){
             $(e.target).parents('tr').html('<td></td>\
     <td><input class="inputEditNew" value="'+event.name+'"></td>\
     <td><button class="btn btn-danger deleteTr" >DELETE</button>\
-    <button class="btn btn-success">Save</button></td>');
-    
-    
-    })
-    
-    
+    <button class="btn btn-success">Save</button></td>');  
+    })  
+})
+
+//function editing product
+$('body').on('click', '.editTr', function(e){
+    let trId = $(e.target).parents('tr').data('id');
+    let nameEdit = $(e.target).parents('tr').children('td').eq(1);
+    let namePrice = $(e.target).parents('tr').children('td').eq(2);
+    let nameDescription = $(e.target).parents('tr').children('td').eq(3);
+    let nameCategories = $(e.target).parents('tr').children('td').eq(4);
+    $(e.target).parents('tr').children('td').eq(5).html('<button class="btn btn-danger deleteTr" >DELETE</button>\
+    <button class="btn btn-success">Save</button></td>'); 
+
+    nameEdit.html( '<input value="'+ nameEdit.html() +'">');
+    namePrice.html( '<input value="'+ namePrice.html() +'">');
+    nameDescription.html( '<input value="'+ nameDescription.html() +'">');
+    //nameCategories.html( '<input value="'+ nameCategories.html() +'">');
+
+    // for(let i =0; i < categories.length; i++){
+    //     let numProd = i +1;
+        
+    //     nameCategories.html('<option value="'+categories[i].id+'">'+ categories[i].name +'</option>')
+    // }
+    //console.log(trId);
+    // $.get('http://localhost:2403/products/'+ trId, function(event){
+    //     //console.log(event.name);
+    // })
 })
 
 loadDataCat();
